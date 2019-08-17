6a61dd579903b1371780e467097e4528e7950466
MasatakaKudou <b1017027@fun.ac.jp>
2019-08-18 05:09:58 +0900

    todo is removed

--- lib/main.dart
+++ lib/main.dart
@@ -33,39 +33,6 @@ class TodoListState extends State<TodoList> {
     }
   }
 
-  Widget _buildTodoList() {
-    return new ListView.builder(
-      itemBuilder: (context, index) {
-        if (index < todoItems.length) {
-          return _buildTodoItem(todoItems[index]);
-        } else {
-          return _buildTodoItem("");
-        }
-      },
-    );
-  }
-
-  Widget _buildTodoItem(String todoText) {
-    return new ListTile(
-        title: new Text(todoText)
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text('Todo List')
-      ),
-      body: _buildTodoList(),
-      floatingActionButton: new FloatingActionButton(
-          onPressed: _pushAddToScreen,
-          tooltip: 'Add task',
-          child: new Icon(Icons.add)
-      ),
-    );
-  }
-
   void _pushAddToScreen() {
     Navigator.of(context).push(
         new MaterialPageRoute(
@@ -88,6 +55,68 @@ class TodoListState extends State<TodoList> {
               );
             }
         )
+    );
+  }
+
+  void removeTodo(int index){
+    setState(() => todoItems.removeAt(index));
+  }
+
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+              title: new Text( todoItems[index] + ' is done?'),
+              actions: <Widget>[
+                new FlatButton(
+                    child: new Text('CANCEL'),
+                    onPressed: () => Navigator.of(context).pop()
+                ),
+                new FlatButton(
+                    child: new Text('DONE'),
+                    onPressed: () {
+                      removeTodo(index);
+                      Navigator.of(context).pop();
+                    }
+                )
+              ]
+          );
+        }
+    );
+  }
+
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < todoItems.length) {
+          return _buildTodoItem(todoItems[index], index);
+        } else {
+          return _buildTodoItem("", 0);
+        }
+      },
+    );
+  }
+
+  Widget _buildTodoItem(String todoText,int index) {
+    return new ListTile(
+        title: new Text(todoText),
+        onTap: () => _promptRemoveTodoItem(index)
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text('Todo List')
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _pushAddToScreen,
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)
+      ),
     );
   }
 
